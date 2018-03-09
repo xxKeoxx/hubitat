d94a6821b6945bb839acfaa932459ff5b6b3e264
xxKeoxx <jprosiak@sbcglobal.net>
2018-03-09 11:18:19 -0600

    Update hueBulbController.sh

--- HAMBridge/hueBulbController.sh
+++ HAMBridge/hueBulbController.sh
@@ -5,5 +5,4 @@ BASEURL="http://10.0.1.2/api/[add api user here]"
 
 LIGHTNAME="Outside_MBR"
 BULBID=`curl -s ${BASEURL}/lights/ | /usr/local/bin/jq -r -e --arg LIGHTNAME "${LIGHTNAME}" '. as $object | keys[] | select($object[.].name == $LIGHTNAME)'`
-#curl -s ${BASEURL}/lights/1 -X PUT -d"{\"on\":true,\"bri\":254,\"hue\":13548,\"sat\":200,\"transitiontime\":50}"
 curl -s ${BASEURL}/lights/${BULBID} -X PUT -d"{\"on\":${TOGGLEMODE},\"bri\":254,\"hue\":13548,\"sat\":200,\"transitiontime\":50}"
