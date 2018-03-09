449932b78ab506a33bf9e54aa9b142ebc029295e
xxKeoxx <jprosiak@sbcglobal.net>
2018-03-09 11:21:42 -0600

    Update hueSceneFinder.sh

--- HAMBridge/hueSceneFinder.sh
+++ HAMBridge/hueSceneFinder.sh
@@ -1,36 +1,12 @@
 #!/bin/bash
 
-GROUPDIR="/Users/jlrosiak/HAM/hue/groups"
-#GROUPID=0
-#RETVAL=0
-BASEURL="http://10.0.1.2/api/43594f081bb6d23e9ccd254927fa47"
+BASEURL="http://[add your hue hub ip here]/api/[add your whitelist user]"
 
 if [ ! -d ${GROUPDIR} ]; then
 	mkdir -p ${GROUPDIR}
 fi
 
-#if [[ -n $1 ]] && [[ $1 != "--help" ]]; then
-#	GROUPID=$1
-#else
-#	GROUPID=[[PARAM1]]
-#fi
-#
-#if [[ -n $2 ]]; then
-#        TOGGLEMODE=$2
-#else
-#        TOGGLEMODE=[[PARAM2]]
-#fi
-#
-#if [[ -n $3 ]]; then
-#	SCENEID=$3
-#else
-#	SCENEID=[[PARAM3]]
-#fi
-
 SCENENAME="KitchenDay"
-
-#curl -s ${BASEURL}/scenes/ | /usr/local/bin/jq -r -e --arg SCENENAME "${SCENENAME}" '. as $object | keys[] | select($object[.].name == $SCENENAME)'
-#curl -s ${BASEURL}/scenes/ | /usr/local/bin/jq -r -e --arg SCENENAME "${SCENENAME}" '.' 
 
 SCENEID=`curl -s ${BASEURL}/scenes -X GET | jq -r -e --arg SCENENAME "${SCENENAME}" '. | keys as $k | $k[] as $myKey |
         if .[$myKey].name == $SCENENAME then
@@ -43,7 +19,3 @@ SCENEID=`curl -s ${BASEURL}/scenes -X GET | jq -r -e --arg SCENENAME "${SCENENAM
         end' | tail -n 1 | awk -F"," '{print $2}'`
 
 echo $SCENEID
-
-#| sed -e 's/[:-]//g' | sort | tail -n 1
-
-#GOUPJSON="{\"scene\":\"${SCENEID}\"}"
