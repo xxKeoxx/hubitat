80906f5dfa6b8034942482d4abe7806428f30799
xxKeoxx <jprosiak@sbcglobal.net>
2018-08-13 00:25:11 -0500

    Update AT&T Digital Life Controller.groovy

--- homecloudhub/AT&T Digital Life Controller.groovy
+++ homecloudhub/AT&T Digital Life Controller.groovy
@@ -33,52 +33,7 @@ metadata {
         attribute "exit-delay", "number"
         attribute "abort-timeout", "number"
 	}
-/*
-    simulator {
-	}
 
-	// UI tile definitions
-	tiles(scale: 2) {
-		standardTile("power-source", "device.power-source", width: 2, height: 2) {
-            state "AC", label: 'AC', icon: "http://cdn.device-icons.smartthings.com/Electronics/electronics14-icn@2x.png", backgroundColor: "#79b821", canChangeIcon: true, canChangeBackground: true
-            state "DC", label: 'DC', icon: "http://cdn.device-icons.smartthings.com/Electronics/electronics14-icn@2x.png", backgroundColor: "#ffa81e", canChangeIcon: true, canChangeBackground: true
-        }
-
-		standardTile("id", "device.id", decoration: "flat", width: 6, height: 1, ) {
-            state "default", label: '${currentValue}', backgroundColor: "#808080", icon:"http://cdn.device-icons.smartthings.com/Electronics/electronics14-icn@2x.png"
-        }
-
-		multiAttributeTile(name:"multi", type:"generic", width:6, height:4) {
-			tileAttribute("device.power-source", key: "PRIMARY_CONTROL") {
-      			attributeState "AC", label:'AC POWER', icon:"http://cdn.device-icons.smartthings.com/Appliances/appliances17@2x.png", backgroundColor:"#79b821"
-      			attributeState "DC", label: 'ON BATTERY', icon:"http://cdn.device-icons.smartthings.com/Appliances/appliances17@2x.png", backgroundColor:"#ffa81e"
-    		}
-    		tileAttribute("device.battery", key: "SECONDARY_CONTROL") {
-      			attributeState "default", label: 'Battery ${currentValue}%', icon:"st.unknown.unknown", backgroundColor:"#ffa81e", unit:"%"
-			}
-		}
-
-		standardTile("lan", "device.connectivity", width: 2, height: 2) {
-            state "0", label: 'LAN ERROR', icon: "st.Weather.weather8", backgroundColor: "#ff381e"
-            state "1", label: 'LAN ERROR', icon: "st.Weather.weather8", backgroundColor: "#ff381e"
-            state "2", label: 'LAN OK', icon: "st.Weather.weather8", backgroundColor: "#79b821"
-            state "3", label: 'LAN OK', icon: "st.Weather.weather8", backgroundColor: "#79b821"
-        }
-
-		standardTile("ip", "device.header-ip", decoration: "flat", width: 2, height: 2) {
-            state "default", label: '${currentValue}', icon: "st.Weather.weather8"
-        }
-
-		standardTile("wan", "device.connectivity", width: 2, height: 2) {
-            state "0", label: 'WAN ERROR', icon: "st.Weather.weather15", backgroundColor: "#ff381e"
-            state "1", label: 'WAN OK', icon: "st.Weather.weather15", backgroundColor: "#79b821"
-            state "2", label: 'WAN ERROR', icon: "st.Weather.weather15", backgroundColor: "#ff381e"
-            state "3", label: 'WAN OK', icon: "st.Weather.weather15", backgroundColor: "#79b821"
-        }
-
-		main(["power-source"])
-        details(['id', 'multi', 'lan', 'ip', 'wan'])
-	}*/
 }
 
 // parse events into attributes
