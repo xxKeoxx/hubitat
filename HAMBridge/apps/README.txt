64017e51efb70da4ad1a36bb0855b5b044f70c4a
xxKeoxx <jprosiak@sbcglobal.net>
2024-10-12 23:23:53 -0500

    Delete HAMBridge directory

--- HAMBridge/apps/README.txt
+++
@@ -1,1 +0,0 @@
-While these apps were written for use with HAMBridge (http://solutionsetcetera.com/about-ham-bridge.html), they could technically be used for interacting with any web server.
--- HAMBridge/hue/hueBulbController.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-BASEURL="http://[add your hue hub ip here]/api/[add your whitelist user]"
-
-LIGHTNAME="Outside_MBR"
-BULBID=`curl -s ${BASEURL}/lights/ | /usr/local/bin/jq -r -e --arg LIGHTNAME "${LIGHTNAME}" '. as $object | keys[] | select($object[.].name == $LIGHTNAME)'`
-curl -s ${BASEURL}/lights/${BULBID} -X PUT -d"{\"on\":${TOGGLEMODE},\"bri\":254,\"hue\":13548,\"sat\":200,\"transitiontime\":50}"
